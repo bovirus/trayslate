bcac0523d267ff4a8a90e7bb8206e76e2a1bfd22
Alexander Tverskoy <astverskoy@gmail.com>
2026-05-16 00:59:03 +0300

    Add rounded corners to the button form

--- forms/formbutton.pas
+++ forms/formbutton.pas
@@ -12,7 +12,7 @@ interface
 
 uses
   {$IFDEF WINDOWS}
-  Windows,
+  Windows,      // Required for CreateRoundRectRgn and SetWindowRgn
   {$ENDIF}
   Classes,
   SysUtils,
@@ -58,12 +58,28 @@ uses mainform, systemtool;
   { TformButtonTrayslate }
 
 procedure TformButtonTrayslate.FormCreate(Sender: TObject);
+{$IFDEF WINDOWS}
+var
+  Rgn: HRGN;
+{$ENDIF}
 begin
+  // Apply localization
   ApplicationTranslate(language, self, formTrayslate.LoadCustomPoFile(formTrayslate.CustomPoFile));
 
+  // Set the appropriate icon based on the theme
   SbTranslate.ImageIndex := ThemeValue(2, 3);
   Width := 27;
   Height := 27;
+
+  // Remove standard window borders to allow custom rounded shape
+  BorderStyle := bsNone;
+
+  // Create a rounded-rectangle region for the form (radius = 6 pixels)
+  {$IFDEF WINDOWS}
+  Rgn := CreateRoundRectRgn(0, 0, Width, Height, 10, 10);
+  SetWindowRgn(Handle, Rgn, True); // The system takes ownership of the region
+  {$ENDIF}
+  // For other platforms, TForm.SetShape with a bitmap can be used instead.
 end;
 
 procedure TformButtonTrayslate.FormShow(Sender: TObject);
@@ -73,22 +89,22 @@ end;
 
 procedure TformButtonTrayslate.FormPaint(Sender: TObject);
 begin
-  Canvas.Pen.Color := clGray;
-  Canvas.Pen.Width := 1;
-  Canvas.MoveTo(Width - 2, 0);
-  Canvas.LineTo(Width - 2, Height - 1);
-  Canvas.MoveTo(0, Height - 2);
-  Canvas.LineTo(Width - 1, Height - 2);
+  Canvas.AntialiasingMode := amOn;
+  Canvas.Pen.Width := 2;
+  Canvas.Brush.Style := bsClear;
 
+  Canvas.Pen.Color := clGray;
+  Canvas.RoundRect(0, 0, Width - 2, Height - 2, 10, 10);
   Canvas.Pen.Color := clSilver;
-  Canvas.MoveTo(Width - 1, 0);
-  Canvas.LineTo(Width - 1, Height);
-  Canvas.MoveTo(0, Height - 1);
-  Canvas.LineTo(Width - 1, Height - 1);
+  Canvas.RoundRect(0, 0, Width - 1, Height - 1, 10, 10);
+
+  Canvas.Pen.Color := clWhite;
+  Canvas.RoundRect(0, 0, Width, Height, 10, 10);
 end;
 
 procedure TformButtonTrayslate.SbTranslateClick(Sender: TObject);
 begin
+  // Hide the button and trigger the translation popup
   TimerHideTimer(Self);
   formTrayslate.TranslatePopup(SourceText, Left, Top);
 end;
@@ -97,22 +113,26 @@ procedure TformButtonTrayslate.CreateParams(var Params: TCreateParams);
 begin
   inherited CreateParams(Params);
   {$IFDEF WINDOWS}
+  // Prevent the form from taking focus (tool window style)
   Params.ExStyle := Params.ExStyle or WS_EX_NOACTIVATE;
   {$ENDIF}
 end;
 
 procedure TformButtonTrayslate.SbTranslateMouseEnter(Sender: TObject);
 begin
+  // Keep the button visible while the mouse is over it
   TimerHide.Enabled := False;
 end;
 
 procedure TformButtonTrayslate.SbTranslateMouseLeave(Sender: TObject);
 begin
+  // Resume auto-hide timer when the mouse leaves
   TimerHide.Enabled := True;
 end;
 
 procedure TformButtonTrayslate.TimerHideTimer(Sender: TObject);
 begin
+  // Automatically hide the form after a timeout
   TimerHide.Enabled := False;
   Hide;
 end;
